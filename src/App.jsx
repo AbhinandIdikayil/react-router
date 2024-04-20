9d94576c3ba8ad527304cdef5106989164d4ee25
Abhinand <abhin4nd6@gmail.com>
2024-04-20 11:40:57 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='react-router'>
       
       <Routes  >
         <Route path='/' element={<Layout />} >
